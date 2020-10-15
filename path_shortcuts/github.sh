01dfc8a887528df697957f817185d688a81d4348
Alexis Lucattini <alexis.lucattini@umccr.org>
2020-10-15 18:46:25 +1100

    Added 'path' shortcuts with configuration
    Swapped git_go_to with go_to_git

--- path_shortcuts/github.sh
+++ path_shortcuts/github.sh
@@ -6,18 +6,9 @@ go_to <tab><tab> then yields all of the git repos we can find in the GITHUB_PATH
 # TODO app-spec find -maxdepth 2 -type f -name '.git' etc.
 '
 
-GITHUB_PATH=""
-
-_go_to() {
-  # TODO - check if folder exists first
-  cd "$1"
-}
-
-go_to_infrastructure() {
-
-  cd "${GITHUB_PATH}/UMCCR/infrastructure"
-}
-
-go_to_cwl_iap() {
-  cd "${GITHUB_PATH}/UMCCR-ILLUMINA/cwl-iap"
+go_to_git() {
+  : '
+  Prepend the GITHUB_PATH with the auto_completion
+  '
+  cd "${GITHUB_PATH}/$1" || return
 }
